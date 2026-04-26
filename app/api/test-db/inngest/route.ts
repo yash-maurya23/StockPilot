28c88a4c7d250a060a2cca636f347c6b83f32a70
Yash <yash.ash12629@gmail.com>
2026-04-27 02:10:42 +0530

    fixed error in inngest

--- app/api/test-db/inngest/route.ts
+++ app/api/test-db/inngest/route.ts
@@ -1,11 +1,8 @@
 import {serve} from "inngest/next";
-import { Inngest } from "inngest";
-import { inngest } from "@/lib/inngest/client";
-import { sendSignUpEmail } from "@/lib/inngest/function";
+import {inngest} from "@/lib/inngest/client";
+import {sendSignUpEmail} from "@/lib/inngest/function";
 
-
-export const {GET,POST,PUT}=serve({
- client:inngest,
- functions:[sendSignUpEmail],
-
+export const { GET, POST, PUT } = serve({
+    client: inngest,
+    functions: [sendSignUpEmail],
 })
